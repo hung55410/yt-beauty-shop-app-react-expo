a4413a089eb1533da5fd1bda2a88aa96882264f0
nguyenhung <hung55410@gmail.com>
2023-09-20 09:48:53 +0700

    Config React navigation

--- app/App.js
+++ app/App.js
@@ -1,11 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {HomeScreen} from "./screens"
+
+const Stack = createNativeStackNavigator();
+
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl text-red-500">App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
