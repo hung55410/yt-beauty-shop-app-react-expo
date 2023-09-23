175b48ec1005a97282ca201ac0696c160d719f10
nguyenhung <hung55410@gmail.com>
2023-09-23 13:38:46 +0700

    Search UI

--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -1,11 +1,53 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { 
+  View, 
+  Text, 
+  SafeAreaView, 
+  Image, 
+  TextInput,
+  TouchableOpacity
+} from 'react-native'
+import React, { useState } from 'react'
+
+import { MaterialIcons } from '@expo/vector-icons';
+import { Screen3 } from '../assets';
 
 const HomeScreen = () => {
+
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const handleSearchTerm = (text) => {
+    setSearchTerm(text)
+  }
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 items-center justify-start bg-[#EBEAEF]">
+      <View className="w-full flex-row items-center justify-between px-4 py-2">
+        <MaterialIcons name="chevron-left" size={32} color="#555" />
+
+        <Image 
+          source={Screen3} 
+          className="w-12 h-12 rounded-xl" 
+          resizeMode='cover'
+        />
+      </View>
+
+      {/* Search Box starts here */}
+      <View className="flex-row items-center justify-between px-4 py-2 w-full space-x-6">
+          <View className="px-4 py-2 bg-white rounded-xl flex-1 flex-row items-center justify-center space-x-2">
+            <MaterialIcons name="search" size={24} color="#7f7f7f" />
+            <TextInput 
+              className="text-base font-semibold text-[#555] flex-1 py-1 mt-1" 
+              placeholder='Search here...'
+            value={searchTerm}
+            onChangeText={handleSearchTerm}
+            />
+          </View>
+        <TouchableOpacity className="w-12 h-12 rounded-xl flex items-center justify-center bg-white">
+          <MaterialIcons name="filter-alt" size={24} color="#7f7f7f" />
+        </TouchableOpacity>
+      </View>
+      {/* Search bbox ends here */}
+    </SafeAreaView>
   )
 }
 
